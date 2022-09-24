340a19209e1950d4bca4af8f813c3fe82eb98ae9
Vladyslav <vladyslavfilonov@gmail.com>
2022-09-24 09:50:38 +0300

    Update homework 17.09.py

--- homework 17.09.py
+++ homework 17.09.py
@@ -18,5 +18,18 @@ while i <= N:
         j += 1
     print()
     i += 1
-
+#5
+N = int(input('Введите ширину треугольника: '))
+i = N
+while i >= 1:
+    j = N
+    while j > i:
+        print(' ', end='')
+        j -= 1
+    k = 1
+    while k <= i:
+        print('*', end='')
+        k += 1
+    print()
+    i -= 1
 
