ced27fcd3da5c7662424d2ad2256b16cfd639e2f
Vladyslav <vladyslavfilonov@gmail.com>
2022-09-28 19:00:11 +0300

    1

---
+++ homework 21.09.py
@@ -0,0 +1,38 @@
+# 1
+
+str1 = '5 4 3 2 1'
+for value in [str1]:
+    print(value)
+print()
+
+# 2
+
+for i in range(1, 11):
+    print(f'3*{i}={3 * i}')
+print()
+
+# 3
+
+# 4
+
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(f'{i * j:4}', end=' ')
+    print()
+print()
+
+# 5
+
+symbol = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя01234567890123456789'
+input1 = input("Введите строку: ")
+sdvig = int(input('Введите сдвиг: '))
+output = ''
+for i in input1:
+    char = symbol.find(i)
+    new_symbol = char + sdvig
+    if i in symbol:
+        output += symbol[new_symbol]
+    else:
+        output += i
+print(output)
+print()
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 """
 1) Пользователя вводит два числа A и B, нужно вывести сумму чисел в диапазоне от A до B.
 2) Пользователь вводит два числа A и B, нужно вывести сумму всех четных чисел в диапазоне от A до B.
@@ -63,18 +62,124 @@ for i in range(0, rows):
         print("* ", end="")
     print("")
 
+
+A = int(input('Введите число А: '))
+B = int(input('Введите число B: '))
+summ = 0
+
+while A < B:
+    summ += A
+    A += 1
+print(summ)
+
+for i in range(A, B):
+    summ += i
+print(summ)
+
+s1 = int(input('Type a number from 1 to 10: '))
+for i in range(1, 11):
+    print(f'{s1}*{i}={s1 * i}')
+    
+s1 = 3
+for i in range(1, 11):
+    s2 = s1 * i
+    print(f'3*{i}={s2}')
+    
+for i in range(1, 11):
+    for j in range(1, 11):
+        s1 = i, i * j
+        print(s1)
+exit()
+n = int(input())
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print(i * j, " " * (5 - len(str(i * j))), end=" ")
+    print("\n", end="")
+    
+for i in range(1, 11):
+    print(f'{i}: ')
+    for j in range(1, 11):
+        print(f'{i} * {j} = {i * j}')
+exit()
+
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(f'{i * j:2}', end=' ')
+    print()
+exit()
+
+symbol = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя01234567890123456789'
+input1 = input("Введите строку: ")
+sdvig = int(input('Введите сдвиг: '))
+output = ''
+for i in input1:
+    char = symbol.find(i)
+    new_symbol = char + sdvig
+    if i in symbol:
+        output += symbol[new_symbol]
+    else:
+        output += i
+print(output)
+print()
 '''
 
-N = int(input('Введите ширину треугольника: '))
-i = 1
-while i <= N:
-    j = i
-    while j < N:
-        print(' ', end='')
-        j += 1
-    k = 1
-    while k <= i:
-        print('*', end='')
-        k += 1
-    print()
+#  ЗАДАНИЕ 0
+"""
+Написать программу которая проверяет что в строке есть хотя бы один пробел, число, буква нижнего и верхнего регистра.
+Если это так, то вывести 'YES', иначе 'NO'
+1. isspace
+2. islower
+3. isupper
+4. isdigit
+test_string = 'Hello world123'
+for symbol in test_string:
+    print(symbol)
+    print(symbol.isspace())
+    print(symbol.islower())
+    print(symbol.isupper())
+    print(symbol.isdigit())
+    print('======================')
+"""
+
+'''test_string = 'Hello% world123'
+
+for symbol in test_string:
+    if symbol.isspace() or symbol.islower() or symbol.isupper() or symbol.isdigit():
+        print('YES')
+    else:
+        print('NO')'''
+
+'''
+1. Напишите программу где пользователь вводит число symbol_len, а программа генерирует пароль длинной symbol_len.
+Чем выше будет сложность пароля, тем лучше.
+2. Напишите программу где пользователь вводит пароль, а программа проверяет сложность пароля и выводит свой результат
+в оценочной шкале от 1 до 5.
+3. Напишите программу где пользователь вводит число - count, а программа выводит count чисел фибоначчи.
+
+count = int(input('Введите число: '))
+f1 = f2 = 1
+print(f1, f2, end=' ')
+i = 2
+while i < count:
+    f1, f2 = f2, f1+f2
+    print(f2, end=' ')
     i += 1
+print()
+'''
+# 1
+'''import random
+
+ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
+ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+digits = '0123456789'
+punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+
+all = ascii_lowercase + ascii_uppercase + digits + punctuation
+k = 5
+symbols = random.choices(ascii_lowercase, k)  # Сгенерировали 5 рандомных символов из букв ascii_lowercase
+done_str = ''.join(symbols)(all,k)
+print(done_str)
+chr = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя01234567890123456789'
+'''
+# 4
+
